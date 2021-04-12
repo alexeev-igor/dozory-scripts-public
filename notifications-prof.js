1e8017278724eff095f431e04231212773be8a4c
igor-alexeev <sandro1991@yandex.ru>
2021-04-12 19:44:42 +0300

    Исправил проверку запросов на наличие даты окончания

--- notifications-prof.js
+++ notifications-prof.js
@@ -3,7 +3,7 @@
 // @namespace   dozory
 // @include     http://game.dozory.ru/ajax.html*
 // @include     http://game.dozory.ru/cgi-bin/main.cgi
-// @version     1.11
+// @version     1.12
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
@@ -118,7 +118,7 @@ if (window.location.href.includes('http://game.dozory.ru/ajax.html')){
                 }
     
                 var endDateTime = /\<strtime_end\>(.*?)\<\/strtime_end\>/.exec(this.responseText)
-                if (endDateTime || endDateTime.length > 1){
+                if (endDateTime != null && endDateTime.length > 1){
     
                     endDateTime = endDateTime[1];
                     var splittedDateTime = endDateTime.split(' ');
